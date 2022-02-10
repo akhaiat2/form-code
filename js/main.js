4f7291fe12f36b8b918f43d0cec91b31c07d1680
Anthony Khaiat <92814726+akhaiat2@users.noreply.github.com>
2022-02-10 16:14:02 -0600

    Random-size circles with random colors

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 function setup() {
   createCanvas(innerWidth, innerHeight)
-  background(0)
-  let x = width / 2
-  let y = height / 2
-  let r = width / 4
-  ellipse(x, y, r)
+  background(255,255,255,125);
+  for (var i = 0; i < 100; i++) {
+    let x = random(0,width)
+    let y = random(0,height)
+    let d = random(10, 200)
+    let r = random(100, 255)
+    let g = random(100, 255)
+    let b = random(100, 255)
+    fill(r, g, b)
+    ellipse(x, y, d)
+  }
 }

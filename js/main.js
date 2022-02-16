8ddd48bc42f93b70c20c80c840d5f0366daad955
Anthony Khaiat <92814726+akhaiat2@users.noreply.github.com>
2022-02-15 23:57:27 -0600

    Torus replication

--- js/main.js
+++ js/main.js
@@ -2,22 +2,20 @@ let i = 0;
 let radius = 1;
 
 function setup () {
-  createCanvas(innerWidth, innerHeight);
+  createCanvas(innerWidth, innerHeight, WEBGL);
   background(0);
 }
 
 function draw () {
-  i += 0.2;
+  i+= 0.5;
   radius++;
   let offsetX = width / 2;
   let offsetY = height / 2;
   let x1 = sin(i * 0.75) * radius + offsetX;
   let y1 = cos(i * 0.75) * radius + offsetY;
-  let x2 = sin(i * 0.5) * radius + offsetX;
-  let y2 = cos(i * 0.5) * radius + offsetY;
-  let x3 = sin(i * 0.25) * radius + offsetX;
-  let y3 = cos(i * 0.25) * radius + offsetY;
-  let c = color(Math.random()*255, Math.random()*255, Math.random()*255);
+  let c = color(random(0,255), 0, 0);
   fill(c);
-  triangle(x1,y1,x2,y2,x3,y3);
+  rotateX(frameCount*0.001);
+  rotateY(frameCount*0.001);
+  torus(x1,y1);
 }

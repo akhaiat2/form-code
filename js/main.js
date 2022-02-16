c442f5fbfd35e4138c44244ef03508ebd59e72a4
Anthony Khaiat <92814726+akhaiat2@users.noreply.github.com>
2022-02-15 23:20:18 -0600

    Triangles

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
-function setup() {
-  createCanvas(innerWidth, innerHeight)
-  background(255,255,255,125);
-  for (var i = 0; i < 100; i++) {
-    let x = random(0,width)
-    let y = random(0,height)
-    let d = random(10, 200)
-    let r = random(100, 255)
-    let g = random(100, 255)
-    let b = random(100, 255)
-    fill(r, g, b)
-    ellipse(x, y, d)
-  }
+let i = 0;
+let radius = 1;
+
+function setup () {
+  createCanvas(innerWidth, innerHeight);
+  background(0);
+}
+
+function draw () {
+  i += 0.2;
+  radius++;
+  let offsetX = width / 2;
+  let offsetY = height / 2;
+  let x1 = sin(i * 0.75) * radius + offsetX;
+  let y1 = cos(i * 0.75) * radius + offsetY;
+  let x2 = sin(i * 0.5) * radius + offsetX;
+  let y2 = cos(i * 0.5) * radius + offsetY;
+  let x3 = sin(i * 0.25) * radius + offsetX;
+  let y3 = cos(i * 0.25) * radius + offsetY;
+  let c = color(Math.random()*255, Math.random()*255, Math.random()*255);
+  fill(c);
+  triangle(x1,y1,x2,y2,x3,y3);
 }

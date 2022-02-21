d9347f4962275093c222cd796e11bcb88e7450b4
Anthony Khaiat <92814726+akhaiat2@users.noreply.github.com>
2022-02-21 16:52:21 -0600

    Added hour variable "h" for colors + animation for initial text

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 let i = 0;
 let radius = 1.0;
+
 //3D shapes with WEBGL + Canvas by p5.js
 
 function myFunction() {
@@ -14,16 +15,36 @@ function setup () {
 
 //drawing a torus using draw function p5.js + rotate + sin + cos
 function draw () {
+  let c = color(255,255,255);
+  //let h = hour();
+  let h = 12;
   i+= 0.5;
   radius++;
   let offsetX = width / 2;
   let offsetY = height / 2;
   let x1 = sin(i * 0.5) * radius + offsetX;
   let y1 = cos(i * 0.5) * radius + offsetY;
-  let c = color(random(0,255), random(0,255), random(0,255));
+  let r = 0;
+  let g = 0;
+  let b = 0;
+  if (h < 6 || h >= 19) {
+    r = random(0, 85);
+    g = random(0,85);
+    b = random(0,85);
+  }
+  else if ((h >= 6 && h < 8) || (h >= 16 && h < 19)) {
+    r = random(85, 170);
+    g = random(85, 170);
+    b = random(85, 170);
+  }
+  else {
+    r = random(170, 255);
+    g = random(170, 255);
+    b = random(170, 255);
+  }
+  c = color(r,g,b)
   fill(c);
-  rotateX(frameCount*0.001);
-  rotateY(frameCount*0.001);
-  if (mouseIsPressed === true) cone(x1,y1);
-  else torus(x1,y1);
+  rotateX(frameCount*0.01);
+  rotateY(frameCount*0.01);
+  torus(x1,y1);
 }
